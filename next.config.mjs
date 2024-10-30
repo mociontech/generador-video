27a2a4da2caa4870a858368670a1a1233afb76b8
Maria Alejandra Perdomo <alejandrav3115@gmail.com>
2024-10-30 11:35:39 -0500

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/(.*)', // Aplica a todas las rutas
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline';",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
